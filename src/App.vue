33ce1950873320d2647c07004691331529e32ecb
964269669 <964269669@qq.com>
2019-08-09 11:20:14 +0800

    第三种 项目准备

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <span class="text">ABCDEFG</span>
+    <router-view/>
+  </div>
+</template>
+
+<style scoped>
+.text{
+  font-family: 'Days one';
+  font-size: 20px;
+  color: yellow;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <span class="iconfont iconbookmark span">书签</span>
+    <div class="dd">我是div</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'home',
+  mounted() {
+  },
+  components: {}
+}
+</script>
+<style scoped>
+ .iconbookmark{
+   font-size: 40px;
+   color: yellowgreen;
+ }
+ .dd{
+   color: blue;
+ }
+</style>
+
